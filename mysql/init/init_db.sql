27a880bb9be85dcd344d5c8259d2885a315ff159
jiro4989 <jiroron666@gmail.com>
2018-04-13 21:58:14 +0900

    コンテナ起動時に初期データをMySQLに追加する設定を追加

---
+++ mysql/init/init_db.sql
@@ -0,0 +1,7 @@
+create database test_db;
+use test_db;
+create table users (name varchar(64), password varchar(64));
+insert into users values ("tanaka", "password");
+insert into users values ("suzuki", "p@ssword");
+insert into users values ("ikeda", "passw0rd");
+insert into users values ("yamada", "00000000");
